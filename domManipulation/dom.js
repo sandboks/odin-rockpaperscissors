0838f49cd8d2d740309e6e2b146387edd6777c5e
Sandboks <sandboxasdf@gmail.com>
2023-11-22 13:35:24 +1100

    following tutorial to add simple button javascript

--- domManipulation/dom.js
+++ domManipulation/dom.js
@@ -1,3 +1,26 @@
+function alertFunction() {
+    alert("YAY! YOU DID IT!");
+}
+
+const btn = document.querySelector('#btn');
+btn.addEventListener('click', function (e) {
+    console.log(e);
+    console.log(e.target);
+    e.target.style.background = 'blue';
+});
+
+// buttons is a node list. It looks and acts much like an array.
+const buttons = document.querySelectorAll('button');
+
+// we use the .forEach method to iterate through each button
+buttons.forEach((button) => {
+
+  // and for each one we add a 'click' listener
+  button.addEventListener('click', () => {
+    alert(button.id);
+  });
+});
+
 /*
 Add the following elements to the container using ONLY JavaScript and the DOM methods shown above.
 
